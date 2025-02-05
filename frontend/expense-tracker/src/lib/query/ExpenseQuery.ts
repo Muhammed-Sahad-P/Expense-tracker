08b094427ffd47a8179cb8f7e11189ac44eff00a
Muhammed Sahad P <zanusahad@gmail.com>
2025-02-05 15:33:39 +0530

    update expense page

--- frontend/expense-tracker/src/lib/query/ExpenseQuery.ts
+++ frontend/expense-tracker/src/lib/query/ExpenseQuery.ts
@@ -1,7 +1,7 @@
 "use client";
 
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import { apiClient } from "./api";
+import { apiClient } from "../api";
 
 export const useGetExpenses = () => {
   return useQuery({
