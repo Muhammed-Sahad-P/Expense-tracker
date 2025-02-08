ccffefc43bd86cefb9bd5b124c757c553b390b47
Muhammed Sahad P <zanusahad@gmail.com>
2025-02-08 10:36:30 +0530

    update middleware

--- frontend/expense-tracker/src/middleware.ts
+++ frontend/expense-tracker/src/middleware.ts
@@ -2,28 +2,25 @@ import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
   const token = req.cookies.get("token")?.value;
+  const { pathname } = req.nextUrl;
 
-  const publicRoutes = ["/login", "/register"];
-  const isPublicRoute = publicRoutes.includes(req.nextUrl.pathname);
+  const publicRoutes = ["/", "/login", "/register"];
+  const isPublicRoute = publicRoutes.includes(pathname);
 
-  if (!token && !isPublicRoute) {
+  const protectedRoutes = ["/dashboard", "/transaction"];
+  const isProtectedRoute = protectedRoutes.includes(pathname);
+
+  if (!token && isProtectedRoute) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
 
   if (token && isPublicRoute) {
-    return NextResponse.redirect(new URL("/", req.url));
+    return NextResponse.redirect(new URL("/dashboard", req.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: [
-    "/dashboard",
-    "/profile",
-    "/income",
-    "/expense",
-    "/login",
-    "/register",
-  ],
+  matcher: ["/", "/dashboard", "/transaction", "/login", "/register"],
 };
