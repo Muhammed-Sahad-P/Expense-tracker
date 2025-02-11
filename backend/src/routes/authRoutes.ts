0be396321e3dccd849bcd1668354c53fd31a066d
Muhammed Sahad P <zanusahad@gmail.com>
2025-02-11 14:40:30 +0530

    update zod middleware

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -6,10 +6,12 @@ import {
 } from "../controllers/userController";
 import { authMiddleware } from "../middleware/authMiddleware";
 import errorCatch from "../utils/errorCatch";
+import { loginSchema, registerSchema } from "../utils/zodSchema";
+import { validateData } from "../middleware/zodValidation";
 
 const router = express.Router();
 
-router.post("/login", errorCatch(loginUser));
-router.post("/register", errorCatch(register));
+router.post("/login", validateData(loginSchema), errorCatch(loginUser));
+router.post("/register", validateData(registerSchema), errorCatch(register));
 router.get("/users", errorCatch(authMiddleware), errorCatch(getAllUsers));
 export default router;
